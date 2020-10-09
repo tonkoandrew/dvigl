d32e355aa8dd5d3d38db4a595c4944019dfa17f6
Andrei Tonka <a.tonka@godeltech.com>
2020-10-09 15:06:12 +0300

    Fixed build in Ubuntu 20.04

--- src/dvigl/main.cpp
+++ src/dvigl/main.cpp
@@ -3,29 +3,41 @@
 
 #include <dvigl/render_manager.h>
 
-extern "C" int dvigl_init()
-{
-    Application* app = Application::ptr();
-    if (app->init())
-    {
-        return 0;
-    }
-    return app->get_error_code();
-}
+extern "C" {
+    double __exp_finite(double x) { return exp(x); }
+    double __log_finite(double x) { return log(x); }
+    double __pow_finite(double x, double y) { return pow(x, y); }
 
-extern "C" int dvigl_main()
-{
-    Application* app = Application::ptr();
-    if (app->main_loop())
-    {
-        app->release();
-        return 0;
-    }
-    return app->get_error_code();
-}
+    float __expf_finite(float x) { return expf(x); }
+    float __logf_finite(float x) { return logf(x); }
+    float __powf_finite(float x, float y) { return powf(x, y); }
+    float __acosf_finite(float x) { return acosf(x); }
 
-extern "C" Uint32 dvigl_get_window_id()
-{
-    SDL_Window* w = RenderMgr::ptr()->get_main_window();
-    return SDL_GetWindowID(w);
+    int dvigl_init()
+    {
+        Application* app = Application::ptr();
+        if (app->init())
+        {
+            return 0;
+        }
+        return app->get_error_code();
+    }
+
+    int dvigl_main()
+    {
+        Application* app = Application::ptr();
+        if (app->main_loop())
+        {
+            app->release();
+            return 0;
+        }
+        return app->get_error_code();
+    }
+
+    Uint32 dvigl_get_window_id()
+    {
+        SDL_Window* w = RenderMgr::ptr()->get_main_window();
+        return SDL_GetWindowID(w);
+    }
+
 }
